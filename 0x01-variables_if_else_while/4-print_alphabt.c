1a5895dcad0c3d20f0a73b63618ae077507bcf30
Olonade OLuwatimilehin <olonadeoluwatimilehin@gmail.com>
2024-01-05 04:37:21 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase letters, except for e and q
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
