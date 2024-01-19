dbbd67a9cdaaff821d4cb85f6778e83326be9ec6
Olonade OLuwatimilehin <olonadeoluwatimilehin@gmail.com>
2024-01-19 04:37:45 -0800

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: bytes allocate
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *i;
+
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	else
+		return (i);
+}
