9c9409ef2640816e1d18db784e66894adf95f39e
Olonade OLuwatimilehin <olonadeoluwatimilehin@gmail.com>
2024-01-25 12:34:40 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
  * print_name - a function that prints a name
@@ -10,5 +9,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
+	{
 		f(name);
+	}
 }
