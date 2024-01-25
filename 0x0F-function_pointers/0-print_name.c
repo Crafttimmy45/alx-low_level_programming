f4be62f01cb433b4f87c61ab2598b18808532e13
Olonade OLuwatimilehin <olonadeoluwatimilehin@gmail.com>
2024-01-25 13:05:16 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - a function that prints a name
- * @name: person's name
- * @f: pointer to a function
- * Return: void
+ * print_name - a function that prints a name.
+ * @name: person's name.
+ * @f: pointer to a function.
+ * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {
