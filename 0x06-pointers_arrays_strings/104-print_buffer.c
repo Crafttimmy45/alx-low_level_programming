c359238181b10aa286e5a8c39710469858be1efd
Olonade OLuwatimilehin <olonadeoluwatimilehin@gmail.com>
2024-02-08 05:28:31 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,57 +2,48 @@
 #include <stdio.h>
 
 /**
- * print_line - prints a bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
- * Return: void
- */
-void print_line(char *c, int s, int l)
-{
-	int g, h;
-
-	for (g = 0; g <= 9; g++)
-	{
-		if (g <= s)
-			printf("%02x", c[l * 10 + g]);
-		else
-			printf(" ");
-		if (g % 2)
-			putchar(' ');
-	}
-	for (h = 0; h <= s; h++)
-	{
-		if (c[l * 10 + h] > 31 && c[l * 10 + h] < 127)
-			putchar(c[l * 10 + h]);
-		else
-			putchar('.');
-	}
-}
-
-/**
  * print_buffer - prints a buffer
- * @b: buffer to print
+ * @b:nbuffer
  * @size: size of buffer
  * Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int a;
+	int g, h, i;
 
-	for (a = 0; a <= (size - 1) / 10 && size; a++)
+	g = 0;
+
+	if (size <= 0)
 	{
-		printf("%08x: ", a * 10);
-		if (a < size / 10)
-		{
-			print_line(b, 9, a);
-		}
-		else
-		{
-			print_line(b, size % 10 - 1, a);
-		}
-		putchar('\n');
+		printf("\n");
+		return;
 	}
-	if (size == 0)
-		putchar('\n');
+	while (g < size)
+	{
+		h = size - g < 10 ? size - g : 10;
+		printf("%8x: ", g);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < h)
+				printf("%02x", *(b + g + i));
+			else
+				printf(" ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < h; i++)
+		{
+			int c = *(b + g + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		g += 10;
+	}
 }
