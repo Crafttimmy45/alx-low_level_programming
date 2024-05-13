4d277a5e19a2aeac56ad6180c02894174e482e33
Olonade OLuwatimilehin <olonadeoluwatimilehin@gmail.com>
2024-05-13 05:16:00 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -17,7 +17,35 @@ void print_buffer(char *b, int size)
 
 		for (h = 0; h < 10; h++)
 		{
-			if ((h +
+			if ((h + g) >= size)
+				printf(" ");
+
+			else
+				printf("%02x", *(b + h + g));
+
+			if ((h % 2) != 0 && h != 0)
+				printf(" ");
 		}
+
+		for (h = 0; h < 10; h++)
+		{
+			if ((h + g) >= size)
+				break;
+
+			else if (*(b + h + g) >= 31 &&
+					*(b + h + g) <= 126)
+				printf("%c", *(b + h + g));
+
+			else
+				printf(".");
+		}
+
+		if (g >= size)
+			continue;
+
+		printf("\n");
 	}
+
+	if (size <= 0)
+		printf("\n");
 }
